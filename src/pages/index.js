3d86b537c93270836113f88d299bc5361b59dd79
jmbreche <brecheisen.jacob@gmail.com>
2023-01-13 01:04:28 -0600

    Fixed scrolling bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,9 +23,9 @@ const IndexPage = () => {
 
         destination = destination - event.deltaY
 
-        if(event.deltaY > 0 && position < sections[current] - min_distance) {
+        if(event.deltaY > 0 && destination < sections[current] - min_distance) {
             current = (current == sections.length - 1) ? 0 : current + 1
-        } else if(event.deltaY < 0 && position > sections[current] + min_distance) {
+        } else if(event.deltaY < 0 && destination > sections[current] + min_distance) {
             current = (current == 0) ? sections.length - 1 : current - 1
         } else {
             return
